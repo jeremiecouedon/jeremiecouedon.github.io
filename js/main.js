ab9f416ccb93acbb60f09701c157851b5eaa7687
Jordan Trémoureux <jordan.tremoureux@gmail.com>
2025-09-02 17:17:11 +0200

    maj

--- js/main.js
+++ js/main.js
@@ -2,42 +2,41 @@ function videoSlider() {
   return {
     videos: [
       {
-        title: "Épisode 1: Titre de la vidéo",
-        image: "assets/images/videos/video-1-thumbnail.png",
-        link: "https://youtube.com/video1",
+        title: "Une victoire paysanne et environnementale !",
+        image: "https://img.youtube.com/vi/EIWlnsHsTB8/mqdefault.jpg",
+        link: "https://www.youtube.com/watch?v=EIWlnsHsTB8",
       },
       {
-        title: "Épisode 2: Titre de la vidéo",
-        image: "assets/images/videos/video-2-thumbnail.png",
-        link: "https://youtube.com/video2",
+        title: 'Bande annonce "Le temps d\'une lutte"',
+        image: "https://img.youtube.com/vi/ZH--tN3v0Uk/mqdefault.jpg",
+        link: "https://www.youtube.com/watch?v=ZH--tN3v0Uk",
       },
       {
-        title: "Épisode 3: Titre de la vidéo",
-        image: "assets/images/videos/video-1-thumbnail.png",
-        link: "https://youtube.com/video3",
+        title: "Transmettre sa ferme",
+        image: "https://img.youtube.com/vi/WIOt-sKcwRU/mqdefault.jpg",
+        link: "https://www.youtube.com/watch?v=WIOt-sKcwRU",
       },
       {
-        title: "Épisode 4: Titre de la vidéo",
-        image: "assets/images/videos/video-2-thumbnail.png",
-        link: "https://youtube.com/video2",
+        title:
+          "Transhumance climatique - les abeilles au cœur du bouleversement",
+        image: "https://img.youtube.com/vi/4H4AovBMhoc/mqdefault.jpg",
+        link: "www.youtube.com/watch?v=4H4AovBMhoc",
       },
       {
-        title: "Épisode 5: Titre de la vidéo",
-        image: "assets/images/videos/video-1-thumbnail.png",
-        link: "https://youtube.com/video3",
+        title: 'Bande annonce "Le champ du local"',
+        image: "https://img.youtube.com/vi/OLqjSwXYILk/mqdefault.jpg",
+        link: "https://www.youtube.com/watch?v=OLqjSwXYILk",
       },
       {
-        title: "Épisode 6: Titre de la vidéo",
-        image: "assets/images/videos/video-2-thumbnail.png",
-        link: "https://youtube.com/video2",
-      },
-      {
-        title: "Épisode 7: Titre de la vidéo",
-        image: "assets/images/videos/video-1-thumbnail.png",
-        link: "https://youtube.com/video3",
+        title:
+          "L'eau à la ferme : stratégies paysannes et changement climatique en Bretagne",
+        image: "https://img.youtube.com/vi/JAijq39lEcw/mqdefault.jpg",
+        link: "https://www.youtube.com/watch?v=JAijq39lEcw",
       },
     ],
     currentIndex: 0,
+    dragStartX: null,
+    dragging: false,
 
     get visibleVideos() {
       const totalVideos = this.videos.length;
@@ -49,15 +48,38 @@ function videoSlider() {
     },
 
     nextSlide() {
-      if (this.currentIndex < this.videos.length - 1) {
-        this.currentIndex++;
-      }
+      this.currentIndex = (this.currentIndex + 1) % this.videos.length;
     },
 
     prevSlide() {
-      if (this.currentIndex > 0) {
-        this.currentIndex--;
+      this.currentIndex =
+        (this.currentIndex - 1 + this.videos.length) % this.videos.length;
+    },
+
+    // Drag events
+    onDragStart(e) {
+      this.dragging = true;
+      this.dragStartX =
+        e.type === "touchstart" ? e.touches[0].clientX : e.clientX;
+    },
+    onDragMove(e) {
+      if (!this.dragging) return;
+      const currentX =
+        e.type === "touchmove" ? e.touches[0].clientX : e.clientX;
+      const diff = currentX - this.dragStartX;
+      if (Math.abs(diff) > 50) {
+        // Sensitivity
+        if (diff < 0) {
+          this.nextSlide();
+        } else {
+          this.prevSlide();
+        }
+        this.dragging = false;
       }
+    },
+    onDragEnd() {
+      this.dragging = false;
+      this.dragStartX = null;
     },
   };
 }
